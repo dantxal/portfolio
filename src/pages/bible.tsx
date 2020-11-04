60ec065ee623d9e2c6ef4aed578b948c48c1619e
dantxal <dantxal@gmail.com>
2020-11-04 17:28:33 -0300

    Add projects routes for Bible and Social

--- src/pages/bible.tsx
+++ src/pages/bible.tsx
@@ -1,5 +1,47 @@
 import Layout from '../components/Layout'
 
+import { Info, Technologies } from '../styles/pages/Social'
+
 export default function Bible(): JSX.Element {
-  return <Layout>TEst</Layout>
+  return (
+    <Layout>
+      <Info>
+        <h1>Free Sacred Bible</h1>
+        <p>
+          This is a free app I developed to make it easier for people to have a
+          Bible on their phones. The app is not monetized in any way (no ads, no
+          in-app purchases).
+          <br />
+          <br />
+          The app includes stack router for navigation, real-time search for
+          books and a simple interface.
+          <br />
+          <br />
+          The version of the scriptures included in the app is a brazilian open
+          source translation:{' '}
+          <a href="https://github.com/blivre/BibliaLivre">BLIVRE</a>. I chose to
+          keep the text in memory, given the small size of it when converted to
+          JSON (6.02 MB). With that the app provides a fast user experience,
+          although I might change to a different method once I start introducing
+          more features, the goal is to do so without affecting user experience.
+        </p>
+      </Info>
+      <Technologies>
+        <div>
+          <strong>Technologies</strong>
+          <p>React, React-Native, Styled-components, ESLint, Typescript</p>
+        </div>
+        <div>
+          <strong>Code</strong>
+          <p>Coming soon (not open sourced yet)</p>
+        </div>
+        <div>
+          <strong>Marketplace</strong>
+          <a href="https://play.google.com/store/apps/details?id=com.appbiblia">
+            BRAZILIAN PLAY STORE
+          </a>
+        </div>
+      </Technologies>
+    </Layout>
+  )
 }
--- src/pages/social.tsx
+++ src/pages/social.tsx
@@ -1,6 +1,6 @@
 import Layout from '../components/Layout'
 
-import { Info } from '../styles/pages/Social'
+import { Info, Technologies } from '../styles/pages/Social'
 
 export default function Social(): JSX.Element {
   return (
@@ -8,18 +8,41 @@ export default function Social(): JSX.Element {
       <Info>
         <h1>Relay Social App</h1>
         <p>
-          Lorem ipsum dolor sit ame Lorem ipsum dolor sit ame Lorem ipsum dolor
-          sit ame Lorem ipsum dolor sit ame Lorem ipsum dolor sit ame Lorem
-          ipsum dolor sit ame Lorem ipsum dolor sit ame Lorem ipsum dolor sit
-          ame Lorem ipsum dolor sit ame. Lorem ipsum dolor sit ame Lorem ipsum
-          dolor sit ame Lorem ipsum dolor sit ame.Lorem ipsum dolor sit ame
-          Lorem ipsum dolor sit ame Lorem ipsum dolor sit ame.Lorem ipsum dolor
-          sit ame Lorem ipsum dolor sit ame Lorem ipsum dolor sit ame.Lorem
-          ipsum dolor sit ame Lorem ipsum dolor sit ame Lorem ipsum dolor sit
-          ame.Lorem ipsum dolor sit ame Lorem ipsum dolor sit ame Lorem ipsum
-          dolor sit ame.
+          Social app with Relay is a project that makes use of newest
+          technologies of React and Relay for{' '}
+          <strong>faster data-fetching</strong> and optimized renders for the
+          true &quot;<strong>real-time experience</strong>&quot; of the user.
+          <br />
+          <br />
+          In order to achieve that we make use of features from React 17 and
+          Relay experimental. The result is very fast data-fetching with minimal
+          loading time.
+          <br />
+          <br />
+          One big advantage of using Relay in this case is the{' '}
+          <strong>scalability</strong> of the application given we are able to
+          make changes in a single place (component) and have that be populated
+          in all other places automatically, due to fragments. Plus the
+          optimization of graphql queries with relay-compiler avoids fetching
+          unnecessary data or having a broken query.
         </p>
       </Info>
+      <Technologies>
+        <div>
+          <strong>Technologies</strong>
+          <p>React, Relay, Styled-components, ESLint, Typescript</p>
+        </div>
+        <div>
+          <strong>Code</strong>
+          <a href="https://github.com/dantxal/social-react-relay-ts">
+            GITHUB REPOSITORY
+          </a>
+        </div>
+        <div>
+          <strong>Live</strong>
+          <a href="https://social-react-relay-ts.vercel.app/">VERCEL APP</a>
+        </div>
+      </Technologies>
     </Layout>
   )
 }
